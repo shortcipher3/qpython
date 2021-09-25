d3edf8a514bd925e934e5ae5d8af70c3c5e5d65e
shortcipher3 <chris.jon.hall@gmail.com>
2021-09-25 09:16:27 -0600

    add example usage

---
+++ mental_math_exercises.py
@@ -0,0 +1,11 @@
+#-*-coding:utf8;-*-
+#qpy:3
+#qpy:console
+# I have different exercises to do in different days and
+# exercises to do with my kids as separate scripts
+import mental_math as mm
+
+mm.addition(num_problems=5, pause=15,
+            digits_1=1, digits_2=1)
+#mm.addition(num_problems=5, pause=15,
+#            digits_1=2, digits_2=1)
